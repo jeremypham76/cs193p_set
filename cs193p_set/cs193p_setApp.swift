94ec9df646cfbd0faf0d1ca10b7ee6813660b718
jeremypham76 <jeremypham76@gmail.com>
2022-08-15 15:30:01 -0500

    initiate

---
+++ cs193p_set/cs193p_setApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_setApp.swift
+//  cs193p_set
+//
+//  Created by Jeremy Pham on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_setApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
